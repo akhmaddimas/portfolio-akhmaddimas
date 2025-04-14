2ff3851c60f36b7b9fd7ab3215d0bf86c6f10625
akhmaddimas <144963775+akhmaddimas@users.noreply.github.com>
2025-04-14 22:34:01 +0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.getElementById('burger');
-  const nav = document.getElementById('nav-links');
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
 
-  if (burger && nav) {
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');  // Menampilkan atau menyembunyikan menu
-      burger.classList.toggle('toggle');   // Animasi burger berubah menjadi X
-    });
-  }
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
 });
