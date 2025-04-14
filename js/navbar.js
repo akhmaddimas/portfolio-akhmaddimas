ef55355609b9a5bd788c14e14f636877706bef17
akhmaddimas <144963775+akhmaddimas@users.noreply.github.com>
2025-04-14 22:02:58 +0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,4 +2,15 @@ fetch("navbar.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("navbar-placeholder").innerHTML = data;
+
+    // Tunggu elemen burger & nav selesai dimuat sebelum dipasang event
+    const burger = document.getElementById('burger');
+    const nav = document.getElementById('nav-links');
+
+    if (burger && nav) {
+      burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle'); // Untuk animasi burger jadi X
+      });
+    }
   });
